d06bf86294416eb3dde7053e8d8f4a48498a2bdf
grejman <grejman@hotmail.com>
2017-07-01 17:28:25 +0200

    Update 1.0

---
+++ README.md
@@ -0,0 +1,36 @@
+# Siebel-D3 Bullet Chart
+
+Siebel Open-UI implementation of the d3js Bullet Chart Control using Phisical Wraper.
+
+The chart is used to display the open time of a service request in relation to the Commit Time, Service Calendar Time Open with and without pending times.
+
+* https://d3js.org/
+* https://github.com/mcgovey/D3-Bullet-Chart
+
+# Implementation
+
+To implement the Physical Wraper the following will need to done.
+
+* Load the Physical Wraper "gmD3BulletChartPW.js" to the "..\PUBLIC\scripts\Siebel\custom" folder.
+* Load the Style Sheet "gmD3BulletChart.css to the "..\PUBLIC\files\custom" folder.
+* Load the d3bulletchart folder and files to the "..\PUBLIC\scripts\Siebel\custom" folder.
+  * Note: for the latest version of d3bulletchart please refer to https://github.com/mcgovey/D3-Bullet-Chart Site.
+* Load the d3js folder and files to the "..\PUBLIC\scripts\Siebel\custom" folder.
+  * Note: for the latest version of d3js please refer to https://d3js.org/ Site.
+* On the BusComp create a Text field that returns a json string in the following format:
+
+  [{"title":"","subtitle":"","ranges":[59.30,200],"measures":[59.30, 9],"markers":[119.6]}]
+
+* On your Applet create a control  with the name "gmD3BulletChart".
+* Add the Physical Wraper to the Application under Manifest Administration.
+* Add the Style Sheet to the Application under Manifest Administration.
+
+Note: That I have not completed this control and will be making more changes.  The control is not supported on Tile applets.
+
+# Contributions
+
+Contributions are welcome.
+
+# Example
+
+![Example](/images/sample.png)
---
+++ source/gmD3BulletChartPW.js
@@ -0,0 +1,184 @@
+if (typeof (SiebelAppFacade.gmD3BulletChartPW) === "undefined") {
+  SiebelJS.Namespace("SiebelAppFacade.gmD3BulletChartPW");
+  define("siebel/custom/gmD3BulletChartPW", ["order!siebel/custom/d3js/d3.v3.min","order!siebel/pwinfra"], function () {
+    SiebelAppFacade.gmD3BulletChartPW = (function () {
+
+      var gUtils     = SiebelJS.Dependency("SiebelApp.Utils");
+      var gConstants = SiebelJS.Dependency("SiebelApp.Constants");
+
+        var script = document.createElement('script');
+        script.async = false;
+        script.src = SIEBEL_BUILD + "siebel/custom/d3bulletchart/bullet" + ".js";
+        document.head.appendChild(script);
+        script = null;
+
+      function gmD3BulletChartPW(pm) {
+
+        var gD3SVG    = null;
+        this.GetD3SVG = function()     { return gD3SVG;   };
+        this.SetD3SVG = function(aObj) { gD3SVG = aObj;   };
+
+        var gGlobals  = {
+               margin: {top: 5, right: 40, bottom: 20, left: 120},
+               width: 0,
+               height: 50,
+               chart: null
+            };
+        gGlobals.width  = 560 - gGlobals.margin.left - gGlobals.margin.right;
+        gGlobals.height = 50 -  gGlobals.margin.top  - gGlobals.margin.bottom;
+
+        this.GetGlobals = function()   { return gGlobals; };
+
+        SiebelAppFacade.gmD3BulletChartPW.superclass.constructor.apply(this, arguments);
+      }
+
+      SiebelJS.Extend(gmD3BulletChartPW, SiebelAppFacade.FieldPW);
+
+      gmD3BulletChartPW.prototype.Init = function () {
+        SiebelAppFacade.gmD3BulletChartPW.superclass.Init.apply(this, arguments);
+      }
+
+      gmD3BulletChartPW.prototype.ShowUI = function () {
+
+        SiebelAppFacade.gmD3BulletChartPW.superclass.ShowUI.apply(this, arguments);
+
+        var lEl       = this.GetEl();
+        var lElName   = this.control.GetInputName();
+        var lGlobals  = this.GetGlobals();
+        var lD3SVG    = null;
+        var lData     = [{title:"",subtitle:"",ranges:[0,0],measures:[0,0],markers:[0]}];
+
+        $("[name='" + lElName + "']").replaceWith("<div id='" + lElName + "' name='" + lElName + "'></div>");
+        lGlobals.chart = d3.bullet().width(lGlobals.width).height(lGlobals.height);
+
+        lD3SVG = d3.select("#" + lElName).selectAll("svg")
+                    .data(lData)
+                  .enter().append("svg")
+                    .attr("class", "gm_bullet")
+                    .attr("id",lElName)
+                    .attr("width",  lGlobals.width  + lGlobals.margin.left + lGlobals.margin.right)
+                    .attr("height", lGlobals.height + lGlobals.margin.top  + lGlobals.margin.bottom)
+                  .append("g")
+                    .attr("transform", "translate(" + lGlobals.margin.left + "," + lGlobals.margin.top + ")")
+                    .call(lGlobals.chart);
+
+        var lTitle = lD3SVG.append("g")
+            .style("text-anchor", "end")
+            .attr("transform", "translate(-6," + lGlobals.height / 2 + ")");
+
+        lTitle.append("text")
+            .attr("class", "gm_bullet_title")
+            .text(function(d) { return d.title; });
+
+        lTitle.append("text")
+            .attr("class", "gm_bullet_subtitle")
+            .attr("dy", "1em")
+            .text(function(d) { return d.subtitle; });
+
+        this.SetD3SVG(lD3SVG);
+      }
+
+      gmD3BulletChartPW.prototype.BindEvents = function () {
+        SiebelAppFacade.gmD3BulletChartPW.superclass.BindEvents.apply(this, arguments);
+      }
+
+      gmD3BulletChartPW.prototype.GetValue = function () {
+        var retVal = SiebelAppFacade.gmD3BulletChartPW.superclass.GetValue.apply(this, arguments);
+        return (retVal);
+      }
+
+      gmD3BulletChartPW.prototype.SetValue = function (value, index) {
+
+        SiebelAppFacade.gmD3BulletChartPW.superclass.SetValue.apply(this, arguments);
+
+        var lEl       = this.GetEl(index);
+        var lElName   = this.control.GetInputName();
+        var lD3El     = d3.select($("div#" + lElName));
+        //var lValue    = $.parseJSON('[{"title":"SR170623-032850","subtitle":"New","ranges":[59.30,200],"measures":[59.30, 9],"markers":[119.6]}]');
+        var lValue    = $.parseJSON(value);
+        lValue[0].ranges[1] = parseInt(d3.max([lValue[0].ranges,lValue[0].measures,lValue[0].markers])) + 480;
+
+        this.GetD3SVG().datum(function (d) {
+                                d.ranges   = lValue[0].ranges.map(function (m) {return m / 60;});
+                                d.markers  = lValue[0].markers.map(function (m) {return m / 60;});
+                                d.measures = lValue[0].measures.map(function (m) {return m / 60;});
+                                d.title    = lValue[0].title;
+                                d.subtitle = lValue[0].subtitle;
+                                return d;
+                              }).call(this.GetGlobals().chart.duration(1000));
+
+        this.GetD3SVG().select(".gm_bullet_title").text(lValue[0].title);
+        this.GetD3SVG().select(".gm_bullet_subtitle").text(lValue[0].subtitle);
+      }
+
+      gmD3BulletChartPW.prototype.BeginQuery = function () {
+        // BeginQuery is called when the control should switch to query mode
+        // Add code here that should happen before default processing
+        SiebelAppFacade.gmD3BulletChartPW.superclass.BeginQuery.apply(this, arguments);
+        // Add code here that should happen after default processing
+      }
+
+      gmD3BulletChartPW.prototype.EndQuery = function () {
+        // EndQuery is called when the control should leave query mode
+        // Add code here that should happen before default processing
+        SiebelAppFacade.gmD3BulletChartPW.superclass.EndQuery.apply(this, arguments);
+        // Add code here that should happen after default processing
+      }
+
+      gmD3BulletChartPW.prototype.OpenPopup = function () {
+        // OpenPopup is called when the control receives a signal to open its popup
+        // Add code here that should happen before default processing
+        SiebelAppFacade.gmD3BulletChartPW.superclass.OpenPopup.apply(this, arguments);
+        // Add code here that should happen after default processing
+      }
+
+      gmD3BulletChartPW.prototype.ClosePopup = function () {
+        // ClosePopup is called when the control receives a signal to close its popup.
+        // Add code here that should happen before default processing
+        SiebelAppFacade.gmD3BulletChartPW.superclass.ClosePopup.apply(this, arguments);
+        // Add code here that should happen after default processing
+      }
+
+      gmD3BulletChartPW.prototype.HandleKey = function (el, event, val) {
+        // HandleKey is used to handle key commands.
+        // Add code here that should happen before default processing
+        SiebelAppFacade.gmD3BulletChartPW.superclass.HandleKey.apply(this, arguments);
+        // Add code here that should happen after default processing
+      }
+
+      gmD3BulletChartPW.prototype.SetState = function (state, flag, index) {
+        // SetState is called when the framework needs to set the control state.
+        // state will be one of 
+        //  consts.get("EDITABLE")
+        //  consts.get("ENABLE")
+        //  consts.get("SHOW")
+        //  consts.get("FOCUS")
+        // flag will be true or false
+        // Add code here that should happen before default processing
+        SiebelAppFacade.gmD3BulletChartPW.superclass.SetState.apply(this, arguments);
+        // Add code here that should happen after default processing
+      }
+
+      gmD3BulletChartPW.prototype.EndLife = function () {
+        this.SetD3SVG(null);
+        
+        SiebelAppFacade.gmD3BulletChartPW.superclass.EndLife.apply(this, arguments);
+      }
+
+      gmD3BulletChartPW.prototype.InjectQTPInfo = function (el) {
+        // InjectQTPInfo is used to inject the automation attributes ot (object type), un (UI name), and rn (repository name).
+        // Add code here that should happen before default processing
+        SiebelAppFacade.gmD3BulletChartPW.superclass.InjectQTPInfo.apply(this, arguments);
+        // Add code here that should happen after default processing
+      }
+
+      return gmD3BulletChartPW;
+    }());
+
+    SiebelApp.S_App.PluginBuilder.AttachPW(consts.get("SWE_CTRL_TEXT"), SiebelAppFacade.gmD3BulletChartPW, function (control, objName) {
+			return control.GetName().indexOf('gmD3BulletChartPW') > -1;
+    });
+
+    return "SiebelAppFacade.gmD3BulletChartPW";
+  })
+}
